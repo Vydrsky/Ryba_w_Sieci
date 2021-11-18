9aeec9c88009e093f362777c7dc86b75931aed7e
Vydrsky <vydrsky@gmail.com>
2021-11-18 17:27:17 +0100

    router i strona startowa

---
+++ front/start_front.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        SGDFSGDFHDGFH
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+define('ROOT_PATH', dirname(__FILE__));
+$logicStateArray = array('start'); //ADD NEW STATE WHEN NECESSARY
+$state = "start"; 
+
+if (array_key_exists('state', $_GET)) {
+	if (in_array($_GET['state'], $logicStateArray)) {
+		$state = $_GET['state'];
+	}
+    else{
+        $state = "notfound";
+    }
+}
+
+require(ROOT_PATH . DIRECTORY_SEPARATOR . "back" . DIRECTORY_SEPARATOR . $state . "_back.php");
+require(ROOT_PATH . DIRECTORY_SEPARATOR . "front" . DIRECTORY_SEPARATOR . $state . "_front.php");
