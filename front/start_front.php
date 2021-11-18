f0097c4fcd2478ac37d218d77f0cc9f33c6c1bfe
Vydrsky <vydrsky@gmail.com>
2021-11-18 19:40:21 +0100

    logo, ikonka, szkielet html, podstawowe style

--- front/start_front.php
+++ front/start_front.php
@@ -3,10 +3,30 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <meta name="description" content="Serwis Wędkarski"/>
-        <link rel="stylesheet" href="css/main_styles.css"/>
+        <link rel="stylesheet" href="styles/main_styles.css"/>
         <link rel="icon" href="favicon.ico"/>
     </head>
     <body>
-        
+        <header>
+            <div id="header-image">
+                <img src="images/logo.png"/>
+            </div> 
+            <div id="header-title">Ryba-w-Sieci</div>
+        </header>
+        <nav>
+
+        </nav>
+        <aside>
+
+        </aside>
+        <main>
+
+        </main>
+        <div id="socialMedia">
+
+        </div>
+        <footer>
+
+        </footer>
     </body>
 </html>
