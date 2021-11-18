50e1df07f40dfb5578a31b21d7efdd4bd07f39e8
Vydrsky <vydrsky@gmail.com>
2021-11-18 20:05:14 +0100

    nowe fonty

--- front/start_front.php
+++ front/start_front.php
@@ -3,6 +3,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <meta name="description" content="Serwis Wędkarski"/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,500;1,500&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Bilbo&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="styles/main_styles.css"/>
         <link rel="icon" href="favicon.ico"/>
     </head>
