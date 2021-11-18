713d5cbcf0d32efc9ea39ab18d5df833c3a56e7e
Vydrsky <vydrsky@gmail.com>
2021-11-18 18:25:51 +0100

    ikonka, szablon html

--- front/start_front.php
+++ front/start_front.php
@@ -1,8 +1,12 @@
 <html>
     <head>
-
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="description" content="Serwis Wędkarski"/>
+        <link rel="stylesheet" href="css/main_styles.css"/>
+        <link rel="icon" href="favicon.ico"/>
     </head>
     <body>
-        SGDFSGDFHDGFH
+        
     </body>
 </html>
