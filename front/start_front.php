a5831515e908127ac7b58dac667252eae928fd54
Vydrsky <vydrsky@gmail.com>
2021-11-19 22:08:20 +0100

    css jak ja cie nienawidze, header skonczony

--- front/start_front.php
+++ front/start_front.php
@@ -1,34 +1,44 @@
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <meta name="description" content="Serwis Wędkarski"/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,500;1,500&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Bilbo&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="styles/main_styles.css"/>
-        <link rel="icon" href="favicon.ico"/>
-    </head>
-    <body>
-        <header>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Serwis Wędkarski" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,500;1,500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bilbo&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/main_styles.css" />
+    <link rel="icon" href="favicon.ico" />
+</head>
+
+<body>
+    <header>
+        <div class="inline-header-container-left">
             <div id="header-image">
-                <img src="images/logo.png"/>
-            </div> 
+                <img src="images/logo.png" />
+            </div>
             <div id="header-title">Ryba-w-Sieci</div>
-        </header>
-        <nav>
-
-        </nav>
-        <aside>
-
-        </aside>
-        <main>
-
-        </main>
-        <div id="socialMedia">
-
         </div>
-        <footer>
+        <div class="inline-header-container-right">
+            <div class="main-link">Zaloguj się</div>
+            <div class="main-link">Stwórz Konto</div>
+            <div class="main-link">O Nas</div>
+        </div>
+    </header>
+    <nav>
+        
+    </nav>
+    <aside>
 
-        </footer>
-    </body>
+    </aside>
+    <main>
+
+    </main>
+    <div id="social-media">
+
+    </div>
+    <footer>
+
+    </footer>
+</body>
+
 </html>
