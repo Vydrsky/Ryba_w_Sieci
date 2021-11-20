3db06de56587776beb52b4729c0a10f6e9d3a1bc
Vydrsky <vydrsky@gmail.com>
2021-11-20 23:23:35 +0100

    hiperłącza działają

--- front/start_front.php
+++ front/start_front.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="Serwis Wędkarski" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,500;1,500&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Bilbo&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exo&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/main_styles.css" />
     <link rel="stylesheet" href="fontello/css/fontello.css" />
     <link rel="icon" href="favicon.ico" />
@@ -17,7 +17,7 @@
             <div id="header-image">
                 <img src="images/logo.png" />
             </div>
-            <div id="header-title">Ryba-w-Sieci</div>
+            <div id="header-title">RYBA-W-SIECI</div>
         </div>
         <div class="inline-header-container-right">
             <div class="main-link">Zaloguj się</div>
@@ -27,25 +27,30 @@
     </header>
     <main>
         <div id="image-site-links">
-            <div class="site-link">
-                <img src="images/sklep-link.jpg" />
-                <div class="link-description">ZAKUP SPRZĘTU</div>
-            </div>
+            <a href="https://youtu.be/CHwjL7jVwv0" target="_blank">
+                <div class="site-link">
+                    <img src="images/sklep-link.jpg" />
+                    <div class="link-description">ZAKUP SPRZĘTU</div>
+                </div>
+            </a>
             <a href="https://youtu.be/na3rrDB8MIA" target="_blank">
                 <div class="site-link">
                     <img src="images/konkursy-link.jpg" />
                     <div class="link-description">ZAWODY WĘDKARSKIE</div>
                 </div>
             </a>
-
-            <div class="site-link">
-                <img src="images/borrow-link.jpg" />
-                <div class="link-description">WYPOŻYCZANIE SPRZĘTU</div>
-            </div>
-            <div class="site-link">
-                <img src="images/info-link.jpg" />
-                <div class="link-description">GALERIA ZDOBYCZY UŻYTKOWNIKÓW</div>
-            </div>
+            <a href="https://www.youtube.com/watch?v=qKxfWd79HFI" target="_blank">
+                <div class="site-link">
+                    <img src="images/borrow-link.jpg" />
+                    <div class="link-description">WYPOŻYCZANIE SPRZĘTU</div>
+                </div>
+            </a>
+            <a href="https://www.youtube.com/watch?v=xwUe6zqHPTA" target="_blank">
+                <div class="site-link">
+                    <img src="images/info-link.jpg" />
+                    <div class="link-description">GALERIA ZDOBYCZY UŻYTKOWNIKÓW</div>
+                </div>
+            </a>
         </div>
     </main>
     <div id="social-media-container">
