27179a56a157fd9d7a2ceff45695e78e96454e25
Lukasz <pum3xlr@gmail.com>
2021-11-24 14:26:29 +0100

    wygląd konkretnego newsa

--- front/concrete_news_front.php
+++ front/concrete_news_front.php
@@ -25,14 +25,30 @@
         </div>
         <div class="inline-header-container-right">
             <div class="header-link">Wróć do strony głównej</div>
-
         </div>
     </header>
+    <nav>
+        <ul>
+            <li>Sklep</li>
+            <li>Sprzęt Używany</li>
+            <li>Galeria</li>
+            <li>Newsy</li>
+        </ul>
+    </nav>
     <main>
         <section>
             <div id="item">
                 <div id="item-title">
                     BLACKOUT!
+                </div>
+                <div id="item-image">
+                    <img src="images/promocja.jpg" />
+                </div>
+                <div id="autor-info">
+                    Opublikowano 24.11.2021r. przez Mateusz Szef Kowalski
+                </div>
+                <div id="item-content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis quam quis ligula laoreet porta. Phasellus aliquet vel orci a suscipit. Vivamus aliquet, urna vitae dapibus condimentum, velit erat bibendum velit, nec dignissim libero metus et arcu. Nulla ut risus id massa imperdiet venenatis. Fusce hendrerit pretium pulvinar. Quisque sodales tincidunt arcu, ac gravida mauris vehicula sodales. Proin hendrerit egestas tortor nec rhoncus. Etiam sit amet dictum eros. Quisque sed risus id nibh interdum fringilla et et metus. Donec lorem augue, hendrerit nec ligula id, scelerisque placerat elit. Ut at nisi elementum, volutpat metus id, laoreet nunc. Aenean facilisis cursus metus sit amet laoreet. Donec bibendum, lacus non euismod vestibulum, orci velit cursus orci, sed mattis metus turpis eget neque.
                 </div>
             </div>
         </section>
