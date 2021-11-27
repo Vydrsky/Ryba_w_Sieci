931ee026394d450a9df6220250351b778c0340d5
spery20 <mateo10012@interia.pl>
2021-11-27 09:37:57 +0100

    zmiana miejsca plikow

--- front/o_nas_front.php
+++ front/o_nas_front.php
@@ -72,13 +72,13 @@
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="d-block w-30 " src="photos/pobrane.jpg" alt="First slide">
+                        <img class="d-block w-30 " src="images/pobrane.jpg" alt="First slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-30 " src="photos/pobrane3.jpg" alt="Second slide">
+                        <img class="d-block w-30 " src="images/pobrane3.jpg" alt="Second slide">
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-30 " src="photos/pobrane2.jpg" alt="Third slide">
+                        <img class="d-block w-30 " src="images/pobrane2.jpg" alt="Third slide">
                     </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
