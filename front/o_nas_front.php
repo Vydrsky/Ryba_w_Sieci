0a45f9c250f6399e92c08784b2e0993831d436e4
spery20 <mateo10012@interia.pl>
2021-11-27 11:16:54 +0100

    dodaenie headerow i footerow , menu jako inlcude

---
+++ front/o_nas_front.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O nas</title>
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Serwis Wędkarski" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,500;1,500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exo&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/main_styles.css" />
+    <link rel="stylesheet" href="styles/o_nas_styles.css" />
+    <link rel="stylesheet" href="fontello/css/fontello.css" />
+    <link rel="icon" href="favicon.ico" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        
+    </style>
+</head>
+
+<body>
+<?php
+    include "includes/header.php";
+    ?>
+    <summary>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis quam quis ligula laoreet porta. Phasellus aliquet vel orci a suscipit. Vivamus aliquet, urna vitae dapibus condimentum, velit erat bibendum velit, nec dignissim libero metus et arcu. Nulla ut risus id massa imperdiet venenatis. Fusce hendrerit pretium pulvinar. Quisque sodales tincidunt arcu, ac gravida mauris vehicula sodales. Proin hendrerit egestas tortor nec rhoncus. Etiam sit amet dictum eros. Quisque sed risus id nibh interdum fringilla et et metus. Donec lorem augue, hendrerit nec ligula id, scelerisque placerat elit. Ut at nisi elementum, volutpat metus id, laoreet nunc. Aenean facilisis cursus metus sit amet laoreet. Donec bibendum, lacus non euismod vestibulum, orci velit cursus orci, sed mattis metus turpis eget neque.</p>
+    </summary>
+    <main>
+        <div id="container">
+            <h1>O nas</h1>
+
+
+
+
+
+            <div id="map-addr">
+                <div id="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2547.034268867592!2d18.789220815929138!3d50.3286119040172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47112ded8bad65af%3A0x92bd7b7e54554b4c!2sZabrze!5e0!3m2!1spl!2spl!4v1637592958532!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+                <div id="addr">
+                    Champions SUPLE SHOP Ernest Bijak</p>
+                    </p>
+                    ADRES SKLEPU:</p>
+                    ul. Mickiewicza 27</p>
+                    15-213 Białystok</p>
+                    </p>
+                    GODZINY OTWARCIA:</p>
+                    Wtorek-Piątek: 10:30-17:30</p>
+                    Sobota: 10:00-15:00</p>
+                </div>
+            </div>
+            <div id="text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus commodi, voluptate odio minus dignissimos qui ipsam officiis aspernatur sint cum amet nemo mollitia deserunt hic, quisquam esse, beatae impedit aut rerum dolorem vel fugiat. Facere quam iure aspernatur. Laudantium, repellat reiciendis atque eveniet eaque quibusdam suscipit, rerum perferendis aliquam mollitia facilis. Esse aperiam dolorem minus odio provident fugiat mollitia modi distinctio eveniet ipsum veniam, officiis autem quia ut ullam ipsam voluptatibus molestiae.</p>
+                Sapiente, non eaque perferendis voluptate nam aspernatur quidem odit. Ducimus debitis deleniti repellat, inventore molestias distinctio enim atque delectus error sunt, eos vitae voluptate, tempora adipisci esse. Fugiat, quos dolorum ab dignissimos illum corrupti, excepturi dolore modi sed odit perspiciatis ipsum ratione earum non veniam praesentium provident nulla ullam commodi omnis maiores doloribus neque eaque. Omnis voluptas et maxime nulla repudiandae enim, accusamus dolorum. Magni minus facere accusamus esse fuga reprehenderit.</p>
+                Facere odit ad impedit natus voluptatibus quae placeat libero alias sed necessitatibus, distinctio, eos nisi totam amet perferendis, sit numquam fugit asperiores repellendus delectus dolorem soluta magni autem voluptas? Quos eligendi iste dolor ipsum optio nihil nobis praesentium laboriosam accusamus beatae sint in minima, quod qui, tempore sapiente, quidem facilis saepe! Possimus, ad quasi. Eum labore animi at quisquam est totam, hic itaque optio minus suscipit, accusamus, assumenda pariatur? Debitis nihil ea enim laborum, fuga alias qui dolorem nesciunt! Cumque laborum aspernatur quisquam hic impedit ducimus consequuntur, iure provident, minima cupiditate ullam nulla possimus ad cum exercitationem accusamus iusto excepturi saepe. Voluptate enim, repudiandae provident itaque impedit libero praesentium eaque necessitatibus doloribus sit non officiis! Ex nihil repellendus, qui ut impedit hic sint unde, asperiores mollitia officiis molestiae magnam magni quo provident voluptates cum incidunt. Mollitia veritatis vel quidem atque esse, voluptatum optio voluptatibus culpa molestiae alias sed cum cumque provident vero aspernatur adipisci, saepe exercitationem quo, ex dolorum? A porro nisi facilis autem nemo accusamus adipisci neque quos voluptatibus, iste inventore sint dolorum hic alias fuga eligendi at magnam. Velit odio omnis ipsam, optio non voluptates sint explicabo ullam accusamus eius facere eaque quia illum porro, tempore doloribus eveniet impedit esse placeat! Eius illum laboriosam harum natus praesentium dicta, numquam, vel id quos molestias eveniet at placeat fugit, nulla laborum! Provident accusamus consequuntur excepturi exercitationem quae, repellat omnis tempore tempora, unde sint ipsam tenetur, error laborum aliquam! Iste, rerum quo autem repellat, consequatur minima, porro commodi distinctio perspiciatis explicabo nihil deleniti vel est quasi sapiente! Itaque at eum nam explicabo, possimus voluptatum voluptatibus eligendi asperiores! Similique dolorum delectus, nisi deserunt sequi quos magni rerum numquam eius nostrum perspiciatis sunt repudiandae eveniet! Temporibus asperiores aperiam non, assumenda nostrum dolorem eveniet sed nisi incidunt corrupti ratione, delectus optio quibusdam vitae consequatur doloremque deserunt a voluptatum dolorum id labore qui voluptatibus repellat similique? Modi quisquam odit corrupti dignissimos, aut enim velit itaque reprehenderit, voluptatum possimus cumque nostrum! Cum accusamus, dignissimos exercitationem enim sint, porro unde et est iste, doloremque architecto! Sint necessitatibus perferendis laborum, magni quibusdam culpa non omnis cum molestiae ratione laudantium ex impedit qui! Corrupti, provident suscipit?
+            </div>
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-30 " src="images/pobrane.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-30 " src="images/pobrane3.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-30 " src="images/pobrane2.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+        </div>
+
+
+
+
+    </main>
+
+
+    <?php
+ include "includes/footer.php";
+ ?>
+</body>
+
+</html>
---
+++ includes/menu.php
@@ -0,0 +1,8 @@
+<nav>
+        <ul>
+            <li>Sklep</li>
+            <li>Sprzęt Używany</li>
+            <li>Galeria</li>
+            <li>Newsy</li>
+        </ul>
+    </nav>
