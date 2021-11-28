bdd10771716a9923aff9c58d92ab9dd4125d1733
spery20 <mateo10012@interia.pl>
2021-11-28 17:09:56 +0100

    gitarra

--- front/login_front.php
+++ front/login_front.php
@@ -1,39 +1,40 @@
 <html>
+
 <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/main_styles.css" />
+    <link rel="stylesheet" href="styles/login_styles.css">
 </head>
+
 <body>
-    <div id="content">
-        <h1>Logowanie</h1>
-        <form method="post" action="index.php?state=login">
-            <fieldset class="login">
-                <div id="labels">
-                    <div class="label">
-                        Login:<br />
-                    </div>
-                    <div class="label">
-                        Hasło:<br />
-                    </div>
-                </div>
-                <div id="inputs">
-                    <input type="text" class="text_inputs" name="name" value=<?php if (isset($_SESSION['inputName'])) echo $_SESSION['inputName']; ?>> <br />
-                    <input type="password" class="text_inputs" name="pass"><br />
-                    <input type="submit" value="zaloguj"/>
-                </div>
-                <?php
-                if (isset($_SESSION['nameError'])) {
-                    echo "<span style='color:red;'>".$_SESSION['nameError']."</span><br>";
-                }
-                if (isset($_SESSION['passError'])) {
-                    echo "<span style='color:red;'>".$_SESSION['passError']."</span><br>";
-                }
-                if (isset($_SESSION['ageError'])) {
-                    echo "<span style='color:red;'>". $_SESSION['ageError']."</span><br>";
-                }
-                ?>
-            </fieldset>
-        </form>
+    <?php
+    include "includes/header.php";
+    ?>
+    <div id="main">
+        <div id="inputs">
+            <h4>Login:</h4>
+            <input type="text" class="text_inputs" name="name" value=<?php if (isset($_SESSION['inputName'])) echo $_SESSION['inputName']; ?>> <br />
+            <h4>Hasło:</h4>
+            <input type="password" class="text_inputs" name="pass"><br />
+            <input type="submit" class="btn" value="zaloguj" />
+        </div>
+
+
     </div>
-    <div id="footer">©Footer</div>
+    <?php
+    if (isset($_SESSION['nameError'])) {
+        echo "<span style='color:red;'>" . $_SESSION['nameError'] . "</span><br>";
+    }
+    if (isset($_SESSION['passError'])) {
+        echo "<span style='color:red;'>" . $_SESSION['passError'] . "</span><br>";
+    }
+    if (isset($_SESSION['ageError'])) {
+        echo "<span style='color:red;'>" . $_SESSION['ageError'] . "</span><br>";
+    }
+
+    ?>
+    <?php
+    include "includes/footer.php";
+    ?>
 </body>
+
 </html>
