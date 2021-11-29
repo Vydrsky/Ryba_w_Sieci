4733d87fc344c8940ca07772d1422658d3b979d7
Vydrsky <vydrsky@gmail.com>
2021-11-29 20:17:09 +0100

    naprawione baza 2: electric bogaloo

--- sklep.sql
+++ sklep.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Czas generowania: 28 Lis 2021, 16:56
--- Wersja serwera: 10.4.14-MariaDB
--- Wersja PHP: 7.4.9
+-- Host: localhost
+-- Generation Time: Nov 29, 2021 at 08:08 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `sklep`
+-- Database: `sklep`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `galeria_zdobyczy`
+-- Table structure for table `galeria_zdobyczy`
 --
 
 CREATE TABLE `galeria_zdobyczy` (
@@ -39,7 +39,7 @@ CREATE TABLE `galeria_zdobyczy` (
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `ogloszenia`
+-- Table structure for table `ogloszenia`
 --
 
 CREATE TABLE `ogloszenia` (
@@ -53,7 +53,7 @@ CREATE TABLE `ogloszenia` (
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `produkty_aukcje`
+-- Table structure for table `produkty_aukcje`
 --
 
 CREATE TABLE `produkty_aukcje` (
@@ -68,7 +68,7 @@ CREATE TABLE `produkty_aukcje` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Zrzut danych tabeli `produkty_aukcje`
+-- Dumping data for table `produkty_aukcje`
 --
 
 INSERT INTO `produkty_aukcje` (`id`, `name`, `type`, `state`, `age`, `prize`, `image`, `id_autora`) VALUES
@@ -77,7 +77,7 @@ INSERT INTO `produkty_aukcje` (`id`, `name`, `type`, `state`, `age`, `prize`, `i
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `produkty_sklep`
+-- Table structure for table `produkty_sklep`
 --
 
 CREATE TABLE `produkty_sklep` (
@@ -86,23 +86,24 @@ CREATE TABLE `produkty_sklep` (
   `type` text COLLATE utf8_polish_ci DEFAULT NULL,
   `prize` double NOT NULL,
   `image` text COLLATE utf8_polish_ci DEFAULT NULL,
+  `description` text COLLATE utf8_polish_ci NOT NULL,
   `id_autora` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Zrzut danych tabeli `produkty_sklep`
+-- Dumping data for table `produkty_sklep`
 --
 
-INSERT INTO `produkty_sklep` (`id`, `name`, `type`, `prize`, `image`, `id_autora`) VALUES
-(1, 'Wędka', 'Na węgorze', 130, 'Baza.png', 3),
-(2, 'Wędka szefa', 'Na karasie', 127.42, 'jpg_318-1831.jpg', 3),
-(3, 'xxx', 'x1', 123, 'jpg_318-1831.jpg', 3),
-(4, 'Wędka', 'x2', 123, 'Baza.png', 3);
+INSERT INTO `produkty_sklep` (`id`, `name`, `type`, `prize`, `image`, `description`, `id_autora`) VALUES
+(1, 'Wędka', 'Na węgorze', 130, 'Baza.png', 'Zwykła wędka, nic więcej', 3),
+(2, 'Wędka szefa', 'Na karasie', 127.42, 'jpg_318-1831.jpg', 'Najlepsza wędka na karasie. Karasie wcale nie jedzą guwna', 3),
+(3, 'xxx', 'x1', 123, 'jpg_318-1831.jpg', 'XNXX', 3),
+(4, 'Wędka', 'x2', 123, 'Baza.png', 'xXxXxXxXxXxXx', 3);
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -116,7 +117,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Zrzut danych tabeli `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `email`, `name`, `surname`, `permission`) VALUES
@@ -124,37 +125,37 @@ INSERT INTO `users` (`id`, `login`, `password`, `email`, `name`, `surname`, `per
 (3, 'Jasiu1', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '123@gmail.com', 'Jan', 'Kowalski', 'user');
 
 --
--- Indeksy dla zrzutów tabel
+-- Indexes for dumped tables
 --
 
 --
--- Indeksy dla tabeli `galeria_zdobyczy`
+-- Indexes for table `galeria_zdobyczy`
 --
 ALTER TABLE `galeria_zdobyczy`
   ADD KEY `fkey_galeria_uzytkownicy` (`id_autora`);
 
 --
--- Indeksy dla tabeli `ogloszenia`
+-- Indexes for table `ogloszenia`
 --
 ALTER TABLE `ogloszenia`
   ADD KEY `fkey_ogloszenia_uzytkownicy` (`id_autora`);
 
 --
--- Indeksy dla tabeli `produkty_aukcje`
+-- Indexes for table `produkty_aukcje`
 --
 ALTER TABLE `produkty_aukcje`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fkey_produkty_uzytkownicy` (`id_autora`);
 
 --
--- Indeksy dla tabeli `produkty_sklep`
+-- Indexes for table `produkty_sklep`
 --
 ALTER TABLE `produkty_sklep`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fkey_produktysklep_uzytkownicy` (`id_autora`);
 
 --
--- Indeksy dla tabeli `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
@@ -164,47 +165,47 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT dla tabeli `produkty_aukcje`
+-- AUTO_INCREMENT for table `produkty_aukcje`
 --
 ALTER TABLE `produkty_aukcje`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT dla tabeli `produkty_sklep`
+-- AUTO_INCREMENT for table `produkty_sklep`
 --
 ALTER TABLE `produkty_sklep`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT dla tabeli `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Ograniczenia dla zrzutów tabel
+-- Constraints for dumped tables
 --
 
 --
--- Ograniczenia dla tabeli `galeria_zdobyczy`
+-- Constraints for table `galeria_zdobyczy`
 --
 ALTER TABLE `galeria_zdobyczy`
   ADD CONSTRAINT `fkey_galeria_uzytkownicy` FOREIGN KEY (`id_autora`) REFERENCES `users` (`id`);
 
 --
--- Ograniczenia dla tabeli `ogloszenia`
+-- Constraints for table `ogloszenia`
 --
 ALTER TABLE `ogloszenia`
   ADD CONSTRAINT `fkey_ogloszenia_uzytkownicy` FOREIGN KEY (`id_autora`) REFERENCES `users` (`id`);
 
 --
--- Ograniczenia dla tabeli `produkty_aukcje`
+-- Constraints for table `produkty_aukcje`
 --
 ALTER TABLE `produkty_aukcje`
   ADD CONSTRAINT `fkey_produkty_uzytkownicy` FOREIGN KEY (`id_autora`) REFERENCES `users` (`id`);
 
 --
--- Ograniczenia dla tabeli `produkty_sklep`
+-- Constraints for table `produkty_sklep`
 --
 ALTER TABLE `produkty_sklep`
   ADD CONSTRAINT `fkey_produktysklep_uzytkownicy` FOREIGN KEY (`id_autora`) REFERENCES `users` (`id`);
