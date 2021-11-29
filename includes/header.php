b1e80b2b4e7dc6d98441cb3f87399acf6d6fd99b
Vydrsky <vydrsky@gmail.com>
2021-11-30 00:16:56 +0100

    hiperlacze do profilu, poprawka inlcudeow w galerii

--- includes/header.php
+++ includes/header.php
@@ -1,23 +1,24 @@
-
-    <header>
-        <div class="inline-header-container-left">
-            <a href="index.php?state=start">
-                <div id="header-image">
-                    <img src="images/logo.png" />
-                </div>
-            </a>
-            <div id="header-title">RYBA-W-SIECI</div>
-        </div>
-        <div class="inline-header-container-right">
-            <a href="index.php?state=login">
+<header>
+    <div class="inline-header-container-left">
+        <a href="index.php?state=start">
+            <div id="header-image">
+                <img src="images/logo.png" />
+            </div>
+        </a>
+        <div id="header-title">RYBA-W-SIECI</div>
+    </div>
+    <div class="inline-header-container-right">
+        <a href="index.php?state=login">
             <div class="header-link">Zaloguj się</div>
-            </a>
-            <a href="index.php?state=register">
+        </a>
+        <a href="index.php?state=register">
             <div class="header-link">Stwórz Konto</div>
-            </a>
-            <a href="index.php?state=o_nas">
-                <div class="header-link">O Nas</div>
-            </a>
+        </a>
+        <a href="index.php?state=o_nas">
+            <div class="header-link">O Nas</div>
+        </a>
+        <a href="index.php?state=profile">
             <i class="icon-user"></i>
-        </div>
-    </header>
+        </a>
+    </div>
+</header>
