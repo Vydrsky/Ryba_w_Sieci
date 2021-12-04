d0bec439963470a0d83efd5d8217560e8ad16f26
Vydrsky <vydrsky@gmail.com>
2021-12-04 19:18:52 +0100

    mozliwosc dodania obrazkow do bazy i jednoczesnie do serwera

--- back/galery_back.php
+++ back/galery_back.php
@@ -11,11 +11,11 @@ $result = $query->fetch();
 $_SESSION['username'] = $result['name'];
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $_SESSION['gallery-image-input'] = $_POST['gallery-image'];
+    $_SESSION['gallery-image-input'] = $_FILES['gallery-image'];
     $_SESSION['gallery-description-input'] = $_POST['gallery-description'];
     $_SESSION['gallery-weight-input'] = $_POST['gallery-weight'];
     $validation = true;
-    if (empty($_POST['gallery-image'])) {
+    if (!isset($_FILES['gallery-image'])) {
         $validation = false;
         $_SESSION['galery-image-error'] = "Wybierz zdjęcie";
     }
@@ -28,10 +28,21 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $_SESSION['galery-weight-error'] = "Podaj wagę ryby";
     }
     if ($validation) {
-        //$query = $db->prepare("INSERT INTO galeria_zdobyczy (opis,waga,zdjecie,polubienia,id_autora) VALUES (':opis',:waga,:zdjecie,0,:id_autora)");
-        //$query->bindValue(':id', $_SESSION['userid']);
-        //$query->execute();
-        //$result = $query->fetch();
-        //$_SESSION['username'] = $result['name'];
+        $fileDir = "images/galery/";
+        $extension = pathinfo($_FILES['gallery-image']['name'], PATHINFO_EXTENSION);
+        $filePath = $fileDir.$_SESSION['userid'].sha1(basename($_FILES['gallery-image']['tmp_name'])).".".$extension;
+        echo $filePath;
+        if(move_uploaded_file($_FILES['gallery-image']['tmp_name'],$filePath)){
+            $query = $db->prepare("INSERT INTO galeria_zdobyczy (opis,waga,zdjecie,polubienia,id_autora) VALUES (:opis,:waga,:zdjecie,0,:id_autora)");
+            $query->bindValue(':opis',$_SESSION['gallery-description-input']);
+            $query->bindValue(':waga',$_SESSION['gallery-weight-input']);
+            $query->bindValue(':zdjecie',$filePath);
+            $query->bindValue(':id_autora',$_SESSION['userid']);
+            $query->execute();
+        }
+        else{
+            $_SESSION['galery-image-error'] = "Błąd przy przesyłaniu zdjęcia";
+        }
     }
 }
+    
--- front/galery_front.php
+++ front/galery_front.php
@@ -21,26 +21,26 @@
     <main>
         <section>
             <div id="add-image-text">
-                <p>Witaj <?php echo $_SESSION['username'];?></p>Podziel się z nami swoimi zdobyczami wypełniając formularz<br> Inni użytkownicy mogą polubić twoje zdjęcia<br>Dla najlepszych łowców czekają niesamowite nagrody
+                <p>Witaj <?php echo $_SESSION['username']; ?></p>Podziel się z nami swoimi zdobyczami wypełniając formularz<br> Inni użytkownicy mogą polubić twoje zdjęcia<br>Dla najlepszych łowców czekają niesamowite nagrody
             </div>
-        <fieldset>
-            <div id ="form-description-container">
-                Obrazek:<br>Opis:<br><br><br><br>Waga:<br>
-            </div>
-            <form method="post" action="">
-                <input type="file" name="gallery-image" accept="image/png,image/jpeg"/><br>
-                <textarea name="gallery-description" rows="5"><?php if (isset($_SESSION['gallery-description-input'])) echo $_SESSION['gallery-description-input']; ?></textarea><br>
-                <input type="number" name="gallery-weight" value="<?php if (isset($_SESSION['gallery-weight-input'])) echo $_SESSION['gallery-weight-input']; ?>" min='0' step='0.01'/>kg<br>
-                <?php
-                if (isset($_SESSION['galery-image-error']))
-                    echo '<span style="color:red;">'.$_SESSION['galery-image-error']."</span><br>";
+            <fieldset>
+                <div id="form-description-container">
+                    Obrazek:<br>Opis:<br><br><br><br>Waga:<br>
+                </div>
+                <form method="post" action="index.php?state=galery"  enctype="multipart/form-data">
+                    <input type="file" accept="image/jpeg,image/gif" name="gallery-image" /><br>
+                    <textarea name="gallery-description" rows="5"><?php if (isset($_SESSION['gallery-description-input'])) echo $_SESSION['gallery-description-input']; ?></textarea><br>
+                    <input type="number" name="gallery-weight" value="<?php if (isset($_SESSION['gallery-weight-input'])) echo $_SESSION['gallery-weight-input']; ?>" min='0' step='0.01' />kg<br>
+                    <?php
+                    if (isset($_SESSION['galery-image-error']))
+                        echo '<span style="color:red;">' . $_SESSION['galery-image-error'] . "</span><br>";
                     if (isset($_SESSION['galery-description-error']))
-                    echo '<span style="color:red;">'.$_SESSION['galery-description-error']."</span><br>";
+                        echo '<span style="color:red;">' . $_SESSION['galery-description-error'] . "</span><br>";
                     if (isset($_SESSION['galery-weight-error']))
-                    echo '<span style="color:red;">'.$_SESSION['galery-weight-error']."</span><br>";
-                ?>
-                <input type="submit" value="Prześlij">
-            </form>
+                        echo '<span style="color:red;">' . $_SESSION['galery-weight-error'] . "</span><br>";
+                    ?>
+                    <input type="submit" value="Prześlij">
+                </form>
             </fieldset>
         </section>
         <article>
