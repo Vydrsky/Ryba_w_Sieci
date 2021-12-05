9f80c82141807498f8f316aeb8737f4e67f9da3b
Vydrsky <vydrsky@gmail.com>
2021-12-05 14:24:56 +0100

    podmieniłem dump bazy, bo pokrywały się, tylko w mojej było trohe wiecej

--- sklep.sql
+++ sklep.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2021 at 12:21 AM
+-- Generation Time: Dec 05, 2021 at 02:24 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -28,13 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `galeria_zdobyczy` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL,
   `opis` text COLLATE utf8_polish_ci DEFAULT NULL,
-  `waga` int(11) NOT NULL,
-  `zdjecie` text COLLATE utf8_polish_ci DEFAULT NULL,
+  `waga` float NOT NULL,
+  `zdjecie` text COLLATE utf8_polish_ci NOT NULL,
   `polubienia` int(11) NOT NULL,
   `id_autora` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Dumping data for table `galeria_zdobyczy`
+--
+
+INSERT INTO `galeria_zdobyczy` (`id`, `opis`, `waga`, `zdjecie`, `polubienia`, `id_autora`) VALUES
+(1, 'sdfasd', 2, 'images/galery/67137c1653cf6ff51bcebabc4a4f0056687ed28b4.jpg', 3, 6),
+(8, 'GURBA MOTUR', 69.69, 'images/galery/6f0b948c54708da4fdf8d0ddb01bfe0d56166faa9.jpg', 1, 6),
+(9, 'kremówka dobra', 2137, 'images/galery/6bacae388f94064577e7eb3a739e04a88e4b5501f.png', 3, 6);
 
 -- --------------------------------------------------------
 
@@ -49,6 +58,28 @@ CREATE TABLE `ogloszenia` (
   `opis` text COLLATE utf8_polish_ci DEFAULT NULL,
   `id_autora` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `polubienia`
+--
+
+CREATE TABLE `polubienia` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `postid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `polubienia`
+--
+
+INSERT INTO `polubienia` (`id`, `userid`, `postid`) VALUES
+(7, 6, 8),
+(15, 6, 9),
+(16, 7, 9),
+(17, 7, 8);
 
 -- --------------------------------------------------------
 
@@ -72,7 +103,11 @@ CREATE TABLE `produkty_aukcje` (
 --
 
 INSERT INTO `produkty_aukcje` (`id`, `name`, `type`, `state`, `age`, `prize`, `image`, `id_autora`) VALUES
-(1, 'Spławik', 'Na węgorze', 'nowy', 2, 22, 'logo_nfz.jpg', 3);
+(19, 'na wegorze', 'wędka', 'uzywana', 22, 120, 'none', 4),
+(20, 'na wegorze', 'wędka', 'uzywana', 22, 120, 'none', 4),
+(21, 'na wegorze', 'wędka', 'uzywana', 22, 120, 'none', 4),
+(25, 'na wegorze', 'wędka', 'uzywana', 22, 120, 'none', 6),
+(26, 'na wegorze', 'wędka', 'uzywana', 22, 120, 'none', 6);
 
 -- --------------------------------------------------------
 
@@ -113,16 +148,21 @@ CREATE TABLE `users` (
   `email` text COLLATE utf8_polish_ci DEFAULT NULL,
   `name` text COLLATE utf8_polish_ci DEFAULT NULL,
   `surname` text COLLATE utf8_polish_ci DEFAULT NULL,
-  `permission` text COLLATE utf8_polish_ci NOT NULL DEFAULT '\'user\''
+  `permission` text COLLATE utf8_polish_ci NOT NULL DEFAULT '\'user\'',
+  `points` int(11) NOT NULL,
+  `rank` text COLLATE utf8_polish_ci NOT NULL,
+  `profile_image` text COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `login`, `password`, `email`, `name`, `surname`, `permission`) VALUES
-(2, 'luk1', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'xxx@gmail.com', 'Lukas', 'Raczka', 'user'),
-(3, 'Jasiu1', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '123@gmail.com', 'Jan', 'Kowalski', 'user');
+INSERT INTO `users` (`id`, `login`, `password`, `email`, `name`, `surname`, `permission`, `points`, `rank`, `profile_image`) VALUES
+(3, 'Jan1', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'halibut@gmail.com', 'Jan', 'Polak', 'user', 10000, 'Sum', ''),
+(4, 'test', '4a18e813942d94ffde4f1695884b6e8fdf321d4d', 'karas@gmail.com', 'sdfg', 'huasd', 'user', 13, 'Szprot', 'images/galery/bass.jpg'),
+(6, 'Vydrsky', '4a18e813942d94ffde4f1695884b6e8fdf321d4d', 'szczupak@gmail.com', 'Karol', 'Wydrzyński', 'user', 12323543, 'Sum', 'images/galery/pike.jpg'),
+(7, 'adik', '4a18e813942d94ffde4f1695884b6e8fdf321d4d', 'ryby123@gmail.com', 'adrian', 'kowalski', 'user', 0, '', '');
 
 --
 -- Indexes for dumped tables
@@ -132,6 +172,7 @@ INSERT INTO `users` (`id`, `login`, `password`, `email`, `name`, `surname`, `per
 -- Indexes for table `galeria_zdobyczy`
 --
 ALTER TABLE `galeria_zdobyczy`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fkey_galeria_uzytkownicy` (`id_autora`);
 
 --
@@ -139,6 +180,14 @@ ALTER TABLE `galeria_zdobyczy`
 --
 ALTER TABLE `ogloszenia`
   ADD KEY `fkey_ogloszenia_uzytkownicy` (`id_autora`);
+
+--
+-- Indexes for table `polubienia`
+--
+ALTER TABLE `polubienia`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_userid` (`userid`),
+  ADD KEY `fk_postid` (`postid`);
 
 --
 -- Indexes for table `produkty_aukcje`
@@ -165,10 +214,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `galeria_zdobyczy`
+--
+ALTER TABLE `galeria_zdobyczy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `polubienia`
+--
+ALTER TABLE `polubienia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
 -- AUTO_INCREMENT for table `produkty_aukcje`
 --
 ALTER TABLE `produkty_aukcje`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `produkty_sklep`
@@ -180,7 +241,7 @@ ALTER TABLE `produkty_sklep`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -197,6 +258,13 @@ ALTER TABLE `galeria_zdobyczy`
 --
 ALTER TABLE `ogloszenia`
   ADD CONSTRAINT `fkey_ogloszenia_uzytkownicy` FOREIGN KEY (`id_autora`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `polubienia`
+--
+ALTER TABLE `polubienia`
+  ADD CONSTRAINT `fk_postid` FOREIGN KEY (`postid`) REFERENCES `galeria_zdobyczy` (`id`),
+  ADD CONSTRAINT `fk_userid` FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `produkty_aukcje`
