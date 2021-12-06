832ca04c62694484b840397c0743067ae7112a02
spery20 <mateo10012@interia.pl>
2021-12-06 15:29:26 +0100

    poprawienie o nas

--- front/o_nas_front.php
+++ front/o_nas_front.php
@@ -35,12 +35,15 @@
 </head>
 
 <body>
-    <?php include "includes/header.php"; ?>
-    <?php include "includes/menu.php"; ?>
-    
+    <?php include "includes/header.php";
+    include "includes/menu.php"; ?>
     <summary>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis quam quis ligula laoreet porta. Phasellus aliquet vel orci a suscipit. Vivamus aliquet, urna vitae dapibus condimentum, velit erat bibendum velit, nec dignissim libero metus et arcu. Nulla ut risus id massa imperdiet venenatis. Fusce hendrerit pretium pulvinar. Quisque sodales tincidunt arcu, ac gravida mauris vehicula sodales. Proin hendrerit egestas tortor nec rhoncus. Etiam sit amet dictum eros. Quisque sed risus id nibh interdum fringilla et et metus. Donec lorem augue, hendrerit nec ligula id, scelerisque placerat elit. Ut at nisi elementum, volutpat metus id, laoreet nunc. Aenean facilisis cursus metus sit amet laoreet. Donec bibendum, lacus non euismod vestibulum, orci velit cursus orci, sed mattis metus turpis eget neque.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis quam quis ligula laoreet porta. Phasellus aliquet vel orci a suscipit. Vivamus aliquet, urna vitae dapibus condimentum, velit erat bibendum velit, nec dignissim libero metus et arcu. Nulla ut risus id massa imperdiet venenatis. Fusce hendrerit pretium pulvinar. Quisque sodales tincidunt arcu, ac gravida mauris vehicula sodales. Proin hendrerit egestas tortor nec rhoncus. Etiam sit amet dictum eros. Quisque sed risus id nibh interdum fringilla et et metus. Donec lorem augue, hendrerit nec ligula id, scelerisque placerat elit. Ut at nisi elementum, volutpat metus id, laoreet nunc. Aenean facilisis cursus metus sit amet laoreet. Donec bibendum, lacus non euismod vestibulum, orci velit cursus orci, sed mattis metus turpis eget neque.</p>
+
     </summary>
+    
+ 
+    
     <main>
         <div id="container">
             <h1>O nas</h1>
