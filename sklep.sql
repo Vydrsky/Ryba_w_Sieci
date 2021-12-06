bc96f2d8f7f56beff742409e03858c9c77fa0cf2
Lukasz <pum3xlr@gmail.com>
2021-12-06 22:24:56 +0100

    db

--- sklep.sql
+++ sklep.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 06, 2021 at 09:38 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.0.13
+-- Host: 127.0.0.1
+-- Czas generowania: 06 Gru 2021, 22:23
+-- Wersja serwera: 10.4.14-MariaDB
+-- Wersja PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sklep`
+-- Baza danych: `sklep2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `galeria_zdobyczy`
+-- Struktura tabeli dla tabeli `galeria_zdobyczy`
 --
 
 CREATE TABLE `galeria_zdobyczy` (
@@ -37,18 +37,18 @@ CREATE TABLE `galeria_zdobyczy` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `galeria_zdobyczy`
+-- Zrzut danych tabeli `galeria_zdobyczy`
 --
 
 INSERT INTO `galeria_zdobyczy` (`id`, `opis`, `waga`, `zdjecie`, `polubienia`, `id_autora`) VALUES
 (1, 'sdfasd', 2, 'images/galery/67137c1653cf6ff51bcebabc4a4f0056687ed28b4.jpg', 4, 6),
 (8, 'GURBA MOTUR', 69.69, 'images/galery/6f0b948c54708da4fdf8d0ddb01bfe0d56166faa9.jpg', 3, 6),
-(9, 'kremówka dobra', 2137, 'images/galery/6bacae388f94064577e7eb3a739e04a88e4b5501f.png', 3, 6);
+(9, 'kremówka dobra', 2137, 'images/galery/67137c1653cf6ff51bcebabc4a4f0056687ed28b4.jpg', 3, 6);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ogloszenia`
+-- Struktura tabeli dla tabeli `ogloszenia`
 --
 
 CREATE TABLE `ogloszenia` (
@@ -61,16 +61,17 @@ CREATE TABLE `ogloszenia` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `ogloszenia`
+-- Zrzut danych tabeli `ogloszenia`
 --
 
 INSERT INTO `ogloszenia` (`id`, `title`, `publication_date`, `description`, `image`, `id_autora`) VALUES
-(4, 'BLACK FRIDAY!', '2021-12-06', 'Jeśli szukasz butów na wyprawy wędkarskie w sezonie jesień/zima, mamy dla Ciebie coś specjalnego. Koniecznie sprawdź propozycję od Savage Gear: model Performance. To porządne obuwie zaprojektowane z myślą o trudnym terenie. Możesz je zabrać na ryby, ale też do lasu czy nawet w góry!', 'images/news/1c017a62dcf60a25d77175b201f7e39e4d91687f3.jpg', 1);
+(4, 'BLACK FRIDAY!', '2021-12-06', 'Jeśli szukasz butów na wyprawy wędkarskie w sezonie jesień/zima, mamy dla Ciebie coś specjalnego. Koniecznie sprawdź propozycję od Savage Gear: model Performance. To porządne obuwie zaprojektowane z myślą o trudnym terenie. Możesz je zabrać na ryby, ale też do lasu czy nawet w góry!', 'images/news/17b725cdb9d3c6c25194198103d1cbfa960b9f5c6.jpg', 1),
+(6, 'Obniżka na spławiki', '2021-12-06', 'Doskonale wykonane, stabilne spławiki żywcowe. New Age Float to seria spławików idealnych do zadań specjalnych. Dzięki materiałom wykorzystanym do ich wykonania są niezwykle wytrzymałe na korozję i uderzenia. Ponadto ogromnym autem jest ich duża wyporność oraz pierścień, który ułatwia montowanie zestawów i zastępuje stopery oraz patyki.', 'images/news/1902cc24f917e0b4f80a301d18f5cad6d45d14fc7.jpg', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `polubienia`
+-- Struktura tabeli dla tabeli `polubienia`
 --
 
 CREATE TABLE `polubienia` (
@@ -80,7 +81,7 @@ CREATE TABLE `polubienia` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `polubienia`
+-- Zrzut danych tabeli `polubienia`
 --
 
 INSERT INTO `polubienia` (`id`, `userid`, `postid`) VALUES
@@ -95,7 +96,7 @@ INSERT INTO `polubienia` (`id`, `userid`, `postid`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `produkty_aukcje`
+-- Struktura tabeli dla tabeli `produkty_aukcje`
 --
 
 CREATE TABLE `produkty_aukcje` (
@@ -111,7 +112,7 @@ CREATE TABLE `produkty_aukcje` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `produkty_aukcje`
+-- Zrzut danych tabeli `produkty_aukcje`
 --
 
 INSERT INTO `produkty_aukcje` (`id`, `name`, `type`, `state`, `age`, `prize`, `image`, `description`, `id_autora`) VALUES
@@ -124,7 +125,7 @@ INSERT INTO `produkty_aukcje` (`id`, `name`, `type`, `state`, `age`, `prize`, `i
 -- --------------------------------------------------------
 
 --
--- Table structure for table `produkty_sklep`
+-- Struktura tabeli dla tabeli `produkty_sklep`
 --
 
 CREATE TABLE `produkty_sklep` (
@@ -138,29 +139,29 @@ CREATE TABLE `produkty_sklep` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `produkty_sklep`
+-- Zrzut danych tabeli `produkty_sklep`
 --
 
 INSERT INTO `produkty_sklep` (`id`, `name`, `type`, `prize`, `image`, `description`, `id_autora`) VALUES
-(8, 'Wędka na karpie', 'Wędka', 150, 'images/news/1771438d50be6410db11b4e5fa35e60370fd68209.jpg', 'Idealna wędka na karpie', 1),
-(9, 'Wędka na karasie', 'Wędka', 200, 'images/news/12434ba632f89340cbf3bc63128d79f13e214af31.jpg', 'Idealna wędka na karasie', 1),
-(10, 'Żyłka nylonowa', 'Linka', 20, 'images/news/1a7b2837ca78a6e06c6da69ac6650cde6410133a8.jpg', 'Wytrzymała linka', 1),
-(11, 'Przynęta żywa na szczupaki', 'Przynęta', 20, 'images/news/1ffdc424c0ed62b3089da8db76080a3dc274be296.png', 'Przynęta na króla wód', 1),
-(12, 'Sieć nylonowa', 'Sieć', 100, 'images/news/1b3013217b801bbe70128228d921ea471167b5f83.jpg', 'Mocna sieć na ryby', 1),
-(13, 'Spławik Jaxon 180g', 'Spławik', 30, 'images/news/1d17ea5e595116a47a9693f6fcf9f383b8a08e150.png', 'Spławik o masie 180g', 1),
-(14, 'Kołowrotek spinningowy', 'Kołowrotek', 200, 'images/news/1fc6635f8c5a4ee821eddff98c241cb8386acb5b8.jpg', 'Staromodny kołowrotek spinningowy', 1),
-(15, 'Kurtka rybacka', 'Ubranie', 200, 'images/news/1070d821c94900139332fba3f0473e413f544b724.jpg', 'Wiatroszczelna kurtka na ryby', 1),
-(16, 'Spodnie rybackie', 'Ubranie', 150, 'images/news/16d1b8a472a5439f5c87cb1a23538f340cb969ca5.jpg', 'Spodnie wodoodporne', 1),
-(17, 'Gumowce rybackie', 'Buty', 95, 'images/news/1f2649f1b2844df99475df803e6753d592c742fd1.jpg', 'Wytrzymałe gumowce rybackie', 1),
-(18, 'Krzesło rybackie', 'Siedzenie', 75, 'images/news/10ce521326b5c274ec1bf5431f663c8fc0d362eec.jpg', 'Rozkładane wodoodporne krzesło rybackie', 1),
-(19, 'Luksusowy namiot Gołębiowski', 'Namiot', 1500, 'images/news/143a57497226e91d06a61e1acb44cbae0ef9545d7.jpg', 'Luksusowy namiot o wielu pomieszczeniach i parkiecie tanecznym', 1),
-(20, 'Torba designerska na ryby', 'Plecak', 950, 'images/news/11b2fbd7e5508be25514c65577a91b80ee5b00bd8.jpg', 'Designerska torba rybacka, idealna na zawody wędkarskie.', 1),
-(21, 'Podbierak zwykły', 'Podbierak', 30, 'images/news/1e5f3a8cf958c93251027a0c060a4cd2bb02a5149.png', 'Zwyczajny podbierak na ryby.', 1);
+(8, 'Wędka na karpie', 'Wędka', 150, 'images/products/1771438d50be6410db11b4e5fa35e60370fd68209.jpg', 'Idealna wędka na karpie', 1),
+(9, 'Wędka na karasie', 'Wędka', 200, 'images/products/12434ba632f89340cbf3bc63128d79f13e214af31.jpg', 'Idealna wędka na karasie', 1),
+(10, 'Żyłka nylonowa', 'Linka', 20, 'images/products/1a7b2837ca78a6e06c6da69ac6650cde6410133a8.jpg', 'Wytrzymała linka', 1),
+(11, 'Przynęta żywa na szczupaki', 'Przynęta', 20, 'images/products/1ffdc424c0ed62b3089da8db76080a3dc274be296.png', 'Przynęta na króla wód', 1),
+(12, 'Sieć nylonowa', 'Sieć', 100, 'images/products/1b3013217b801bbe70128228d921ea471167b5f83.jpg', 'Mocna sieć na ryby', 1),
+(13, 'Spławik Jaxon 180g', 'Spławik', 30, 'images/products/1d17ea5e595116a47a9693f6fcf9f383b8a08e150.png', 'Spławik o masie 180g', 1),
+(14, 'Kołowrotek spinningowy', 'Kołowrotek', 200, 'images/products/1fc6635f8c5a4ee821eddff98c241cb8386acb5b8.jpg', 'Staromodny kołowrotek spinningowy', 1),
+(15, 'Kurtka rybacka', 'Ubranie', 200, 'images/products/1070d821c94900139332fba3f0473e413f544b724.jpg', 'Wiatroszczelna kurtka na ryby', 1),
+(16, 'Spodnie rybackie', 'Ubranie', 150, 'images/products/16d1b8a472a5439f5c87cb1a23538f340cb969ca5.jpg', 'Spodnie wodoodporne', 1),
+(17, 'Gumowce rybackie', 'Buty', 95, 'images/products/1f2649f1b2844df99475df803e6753d592c742fd1.jpg', 'Wytrzymałe gumowce rybackie', 1),
+(18, 'Krzesło rybackie', 'Siedzenie', 75, 'images/products/10ce521326b5c274ec1bf5431f663c8fc0d362eec.jpg', 'Rozkładane wodoodporne krzesło rybackie', 1),
+(19, 'Luksusowy namiot Gołębiowski', 'Namiot', 1500, 'images/products/143a57497226e91d06a61e1acb44cbae0ef9545d7.jpg', 'Luksusowy namiot o wielu pomieszczeniach i parkiecie tanecznym', 1),
+(20, 'Torba designerska na ryby', 'Plecak', 950, 'images/products/11b2fbd7e5508be25514c65577a91b80ee5b00bd8.jpg', 'Designerska torba rybacka, idealna na zawody wędkarskie.', 1),
+(21, 'Podbierak zwykły', 'Podbierak', 30, 'images/products/1e5f3a8cf958c93251027a0c060a4cd2bb02a5149.png', 'Zwyczajny podbierak na ryby.', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktura tabeli dla tabeli `users`
 --
 
 CREATE TABLE `users` (
@@ -177,7 +178,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `users`
+-- Zrzut danych tabeli `users`
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `email`, `name`, `surname`, `permission`, `points`, `rank`, `profile_image`) VALUES
@@ -191,7 +192,7 @@ INSERT INTO `users` (`id`, `login`, `password`, `email`, `name`, `surname`, `per
 -- --------------------------------------------------------
 
 --
--- Table structure for table `zawody`
+-- Struktura tabeli dla tabeli `zawody`
 --
 
 CREATE TABLE `zawody` (
@@ -204,25 +205,25 @@ CREATE TABLE `zawody` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for dumped tables
+-- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `galeria_zdobyczy`
+-- Indeksy dla tabeli `galeria_zdobyczy`
 --
 ALTER TABLE `galeria_zdobyczy`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fkey_galeria_uzytkownicy` (`id_autora`);
 
 --
--- Indexes for table `ogloszenia`
+-- Indeksy dla tabeli `ogloszenia`
 --
 ALTER TABLE `ogloszenia`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fkey_ogloszenia_uzytkownicy` (`id_autora`);
 
 --
--- Indexes for table `polubienia`
+-- Indeksy dla tabeli `polubienia`
 --
 ALTER TABLE `polubienia`
   ADD PRIMARY KEY (`id`),
@@ -230,27 +231,27 @@ ALTER TABLE `polubienia`
   ADD KEY `fk_postid` (`postid`);
 
 --
--- Indexes for table `produkty_aukcje`
+-- Indeksy dla tabeli `produkty_aukcje`
 --
 ALTER TABLE `produkty_aukcje`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fkey_produkty_uzytkownicy` (`id_autora`);
 
 --
--- Indexes for table `produkty_sklep`
+-- Indeksy dla tabeli `produkty_sklep`
 --
 ALTER TABLE `produkty_sklep`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fkey_produktysklep_uzytkownicy` (`id_autora`);
 
 --
--- Indexes for table `users`
+-- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `zawody`
+-- Indeksy dla tabeli `zawody`
 --
 ALTER TABLE `zawody`
   ADD PRIMARY KEY (`id`);
@@ -260,78 +261,78 @@ ALTER TABLE `zawody`
 --
 
 --
--- AUTO_INCREMENT for table `galeria_zdobyczy`
+-- AUTO_INCREMENT dla tabeli `galeria_zdobyczy`
 --
 ALTER TABLE `galeria_zdobyczy`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT for table `ogloszenia`
+-- AUTO_INCREMENT dla tabeli `ogloszenia`
 --
 ALTER TABLE `ogloszenia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `polubienia`
+-- AUTO_INCREMENT dla tabeli `polubienia`
 --
 ALTER TABLE `polubienia`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
--- AUTO_INCREMENT for table `produkty_aukcje`
+-- AUTO_INCREMENT dla tabeli `produkty_aukcje`
 --
 ALTER TABLE `produkty_aukcje`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
--- AUTO_INCREMENT for table `produkty_sklep`
+-- AUTO_INCREMENT dla tabeli `produkty_sklep`
 --
 ALTER TABLE `produkty_sklep`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `zawody`
+-- AUTO_INCREMENT dla tabeli `zawody`
 --
 ALTER TABLE `zawody`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- Ograniczenia dla zrzutów tabel
 --
 
 --
--- Constraints for table `galeria_zdobyczy`
+-- Ograniczenia dla tabeli `galeria_zdobyczy`
 --
 ALTER TABLE `galeria_zdobyczy`
   ADD CONSTRAINT `fkey_galeria_uzytkownicy` FOREIGN KEY (`id_autora`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `ogloszenia`
+-- Ograniczenia dla tabeli `ogloszenia`
 --
 ALTER TABLE `ogloszenia`
   ADD CONSTRAINT `fkey_ogloszenia_uzytkownicy` FOREIGN KEY (`id_autora`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `polubienia`
+-- Ograniczenia dla tabeli `polubienia`
 --
 ALTER TABLE `polubienia`
   ADD CONSTRAINT `fk_postid` FOREIGN KEY (`postid`) REFERENCES `galeria_zdobyczy` (`id`),
   ADD CONSTRAINT `fk_userid` FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `produkty_aukcje`
+-- Ograniczenia dla tabeli `produkty_aukcje`
 --
 ALTER TABLE `produkty_aukcje`
   ADD CONSTRAINT `fkey_produkty_uzytkownicy` FOREIGN KEY (`id_autora`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `produkty_sklep`
+-- Ograniczenia dla tabeli `produkty_sklep`
 --
 ALTER TABLE `produkty_sklep`
   ADD CONSTRAINT `fkey_produktysklep_uzytkownicy` FOREIGN KEY (`id_autora`) REFERENCES `users` (`id`);
